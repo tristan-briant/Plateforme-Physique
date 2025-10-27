0348d6568aacfa0c775e19483d540bde2b68de95
tristan-briant <tristan.briant@upmc.fr>
2025-10-27 09:50:06 +0100

    Temperature-controller V0.2

--- M5-Dial-temperature-controller/src/communication.cpp
+++ M5-Dial-temperature-controller/src/communication.cpp
@@ -2,20 +2,19 @@
 // #include <M5Stack.h>
 #include "Vrekrer_scpi_parser.h"
 
-#define __TC_VERSION__ "V0.1"
+#define __TC_VERSION__ "V0.2"
 
 void save_value(const char *name, const char *key, double data);
 void save_tuning(PID pid1);  //, PID pid2);
 void load_tuning(PID *pid1); //, PID *pid2);
 
-extern PID pid1;//, pid2;
+extern PID pid1;
 extern double temp1, temp2, tset1, tset2;
 
 SCPI_Parser my_instrument;
 
 void Identify(SCPI_C commands, SCPI_P parameters, Stream &interface)
 {
-    Serial.println("titi");
     interface.print(F("Temperature Controller, "));
     interface.println(F(__TC_VERSION__));
 }
@@ -48,6 +47,12 @@ void getTunings(SCPI_C commands, SCPI_P parameters, Stream &interface)
         interface.printf("%.5f\n", pidselect->GetKi());
     else if (last_header == "TSET?")
         interface.printf("%.2f\n", pidselect->GetSet());
+    else if (last_header == "DIR?")
+        interface.printf("%.2f\n", pidselect->GetDirection());
+    else if (last_header == "MAX?")
+        interface.printf("%.2f\n", pidselect->GetOutMax());
+    else if (last_header == "MIN?")
+        interface.printf("%.2f\n", pidselect->GetOutMin());
 }
 
 void setTunings(SCPI_C commands, SCPI_P parameters, Stream &interface)
@@ -61,25 +66,16 @@ void setTunings(SCPI_C commands, SCPI_P parameters, Stream &interface)
 
     double newValue = String(parameters[0]).toFloat();
 
-    /*if (parameters.Size() > 1 && String(parameters[1]) == "2")
-        pidselect = &pid2;
-    else
-        pidselect = &pid1;*/
-
     if (last_header == "TSET")
     {
-        if (pidselect == &pid1)
-        {
-            tset1 = newValue;
-            save_value("PID1", "TSET", tset1);
-        }
-        else
-        {
-            tset2 = newValue;
-            save_value("PID2", "TSET", tset2);
-        }
+        tset1 = newValue;
+        // save_value("PID1", "TSET", tset1);
         return;
     }
+    else if (last_header == "MAX")
+        pidselect->SetOutputLimits(pidselect->GetOutMin(), newValue);
+    else if (last_header == "MIN")
+        pidselect->SetOutputLimits(newValue, pidselect->GetOutMax());
     else if (last_header == "GP")
         pidselect->SetTunings(newValue, pidselect->GetKi(), pidselect->GetKd());
     else if (last_header == "GI")
@@ -88,7 +84,7 @@ void setTunings(SCPI_C commands, SCPI_P parameters, Stream &interface)
         pidselect->SetTunings(pidselect->GetKp(), pidselect->GetKi(), newValue);
     else if (last_header.startsWith("DIR"))
         pidselect->SetControllerDirection(newValue > 0 ? 1 : -1);
-    save_tuning(pid1); //, pid2);
+    save_tuning(pid1);
 }
 
 void setLimits(SCPI_C commands, SCPI_P parameters, Stream &interface)
@@ -97,24 +93,20 @@ void setLimits(SCPI_C commands, SCPI_P parameters, Stream &interface)
     last_header.toUpperCase();
     PID *pidselect = &pid1;
 
+    Serial.println("set !");
+
     if (parameters.Size() == 0)
         return;
 
     double newValue = String(parameters[0]).toFloat();
-
-    /*if (parameters.Size() > 1 && String(parameters[1]) == "2")
-        pidselect = &pid2;
-    else
-        pidselect = &pid1;*/
-
     double max, min;
 
-    if (last_header == "OUTMAX")
+    if (last_header == "MAXI")
     {
         min = pidselect->GetOutMin();
         max = constrain(newValue, -100, 100);
     }
-    else if (last_header == "OUTMIN")
+    else if (last_header == "MINI")
     {
         max = pidselect->GetOutMax();
         min = constrain(newValue, -100, 100);
@@ -126,39 +118,43 @@ void setLimits(SCPI_C commands, SCPI_P parameters, Stream &interface)
         return;
 
     pidselect->SetOutputLimits(min, max);
-    /*save_value("PID1", "OUTMAX", pid1.GetOutMax());
-    save_value("PID1", "OUTMIN", pid1.GetOutMin());
-    save_value("PID2", "OUTMAX", pid2.GetOutMax());
-    save_value("PID2", "OUTMIN", pid2.GetOutMin());*/
-
-    save_tuning(pid1); //, pid2);
+    save_tuning(pid1);
 }
 
-void getLimits(SCPI_C commands, SCPI_P parameters, Stream &interface) {}
+void getLimits(SCPI_C commands, SCPI_P parameters, Stream &interface)
+{
+    String last_header = String(commands.Last());
+    last_header.toUpperCase();
+    PID *pidselect = &pid1;
+
+    Serial.println("get !");
+
+    if (last_header == "MINI?")
+        interface.printf("%.5f\n", pidselect->GetOutMin());
+    else if (last_header == "MAXI?")
+        interface.printf("%.5f\n", pidselect->GetOutMax());
+}
 
 void initialize_SCPI()
 {
-    int param = 1;
     my_instrument.RegisterCommand(F("*IDN?"), &Identify);
     my_instrument.RegisterCommand(F("TEMP?"), &getTemp);
+    
     my_instrument.SetCommandTreeBase(F("PID:"));
+    my_instrument.RegisterCommand(F(":TSET"), &setTunings);
     my_instrument.RegisterCommand(F(":GP"), &setTunings);
     my_instrument.RegisterCommand(F(":GD"), &setTunings);
     my_instrument.RegisterCommand(F(":GI"), &setTunings);
-    my_instrument.RegisterCommand(F(":DIRection"), &setTunings);
-    my_instrument.RegisterCommand(F(":OUTMAX"), &setLimits);
-    my_instrument.RegisterCommand(F(":OUTMIN"), &setLimits);
-
+    my_instrument.RegisterCommand(F(":DIR"), &setTunings);
+    my_instrument.RegisterCommand(F(":MAX"), &setTunings);
+    my_instrument.RegisterCommand(F(":MAX?"), &getTunings);
+    my_instrument.RegisterCommand(F(":MIN"), &setTunings);
+    my_instrument.RegisterCommand(F(":MIN?"), &getTunings);
     my_instrument.RegisterCommand(F(":GP?"), &getTunings);
     my_instrument.RegisterCommand(F(":GD?"), &getTunings);
     my_instrument.RegisterCommand(F(":GI?"), &getTunings);
+    my_instrument.RegisterCommand(F(":DIR?"), &getTunings);
     my_instrument.RegisterCommand(F(":TSET?"), &getTunings);
-    my_instrument.RegisterCommand(F(":OUTMAX?"), &getLimits);
-    my_instrument.RegisterCommand(F(":OUTMIN?"), &getLimits);
-
-    /*my_instrument.RegisterCommand(F(":BRIGhtness?"), &GetBrightness);
-    my_instrument.RegisterCommand(F(":BRIGhtness:INCrease"), &IncDecBrightness);
-    my_instrument.RegisterCommand(F(":BRIGhtness:DECrease"), &IncDecBrightness);*/
 }
 
 void loopComunication(void *param)
@@ -170,88 +166,3 @@ void loopComunication(void *param)
         delay(10);
     }
 }
-
-/*void loopComunication(void *param)
-{
-    while (true)
-    {
-        if (Serial.available())
-        {
-            int error = 1;
-            do
-            {
-                const int16_t BUFFER_SIZE = 100;
-                String str(BUFFER_SIZE);
-                str = Serial.readStringUntil('\n');
-                str.toUpperCase();
-
-                if (str == "TEMP1?")
-                    Serial.printf("%.2f\n", temp1);
-                else if (str == "TEMP2?")
-                    Serial.printf("%.2f\n", temp2);
-                else if (str == "*IDN?")
-                    Serial.printf("Temperature Controller %s\n", __TC_VERSION__);
-                else if (str.startsWith("PID"))
-                {
-                    PID *pidselect = NULL;
-
-                    if (str.startsWith("PID1:"))
-                        pidselect = &pid1;
-                    else if (str.startsWith("PID2:"))
-                        pidselect = &pid2;
-                    else
-                        break;
-                    str = str.substring(5);
-                    if (str == "GP?")
-                        Serial.printf("%.5f\n", pidselect->GetKp());
-                    else if (str == "GD?")
-                        Serial.printf("%.5f\n", pidselect->GetKd());
-                    else if (str == "GI?")
-                        Serial.printf("%.5f\n", pidselect->GetKi());
-                    else if (str == "TSET?")
-                        Serial.printf("%.2f\n", pidselect->GetSet()); // pidselect == &pid1 ? tset1 : tset2);
-
-                    else if (str.startsWith("GP "))
-                    {
-                        float x = str.substring(3).toFloat();
-                        pidselect->SetTunings(x, pidselect->GetKi(), pidselect->GetKd());
-                        save_tuning(pid1, pid2);
-                    }
-                    else if (str.startsWith("GI "))
-                    {
-                        float x = str.substring(3).toFloat();
-                        pidselect->SetTunings(pidselect->GetKp(), x, pidselect->GetKd());
-                        save_tuning(pid1, pid2);
-                    }
-                    else if (str.startsWith("GD "))
-                    {
-                        float x = str.substring(3).toFloat();
-                        pidselect->SetTunings(pidselect->GetKp(), pidselect->GetKi(), x);
-                        save_tuning(pid1, pid2);
-                    }
-                    else if (str.startsWith("TSET "))
-                    {
-                        float x = str.substring(5).toFloat();
-                        if (pidselect == &pid1)
-                        {
-                            tset1 = x;
-                            save_value("PID1", "TSET", tset1);
-                        }
-                        else
-                        {
-                            tset2 = x;
-                            save_value("PID2", "TSET", tset2);
-                        }
-                    }
-                }
-
-                error = 0;
-            } while (false);
-
-            if (error)
-                Serial.println("syntaxe error");
-        }
-
-        delay(10);
-    }
-}*/
--- M5-Dial-temperature-controller/src/main.cpp
+++ M5-Dial-temperature-controller/src/main.cpp
@@ -3,112 +3,173 @@
 #include "PID_v1.h"
 #include <DS18B20.h>
 #include "Encoder.h"
+#include "button.h"
+
+// #define DEBUG
 
 char str[100];
 
 ///////  OutPut Pin  //////////
 
-gpio_num_t Pin_PWM1 = GPIO_NUM_13;
-gpio_num_t Pin_PWM2 = GPIO_NUM_15;
+// PORT A
+gpio_num_t Pin_DIR = GPIO_NUM_13;
+gpio_num_t Pin_PWM = GPIO_NUM_15;
+
+// PORT B
 gpio_num_t ISensPin = GPIO_NUM_2;
 DS18B20 ds(GPIO_NUM_1);
 
-const int freqPWM = 20;
-// const int MaxPWM = 4095;
-const int NbitPWM = 11;
+const int freqPWM = 100000;
+const int MaxPWM = 255;
+const int NbitPWM = 8;
 
 bool outputEnable = true;
 
 /// PID variables //////
 double temp1, temp2, tset1 = 20, tset2 = 20;
-double output1, output2;
+double output1;
 
 const double T_factor = 143.0 / 4095.0;
 
-double isens, isens_HR;
+double isens, isens_HR, V_offset;
 float measure = 0;
 
-// int pid_enable = 1;
-
 PID pid1(&temp1, &output1, &tset1, 0, 0, 0, DIRECT);
-// PID pid2(&temp2, &output2, &tset2, 0, 0, 0, DIRECT);
 
 ///////////////// Function declaration ///////////////
 void loopComunication(void *param);
 bool load_value(const char *name, const char *key, double *target);
 void save_value(const char *name, const char *key, double data);
-void save_tuning(PID pid1);  //, PID pid2);
-void load_tuning(PID *pid1); //, PID *pid2);
-///////////////// \Function declaration ///////////////
+void save_tuning(PID pid1);
+void load_tuning(PID *pid1);
+
+uint16_t color565(uint8_t r, uint8_t g, uint8_t b)
+{
+  return (r & 0xF8) << 8 | (g & 0xFC) << 3 | b >> 3;
+}
+
+uint16_t color565(uint8_t grey)
+{
+  return (grey & 0xF8) << 8 | (grey & 0xFC) << 3 | grey >> 3;
+}
+///////////////// Function declaration ///////////////
 
 void loopGUI(void *param)
 {
+  M5Canvas img(&M5.Lcd);
+  Button buttonScreen(0, 0, 240, 240);
+
+  img.createSprite(240, 240);
+  int page = 0;
 
   while (true)
   {
     M5.update();
-    /*int x0 = 120, y0 = 50, r0 = 60;
-    M5.Lcd.fillCircle(x0, y0, r0, DARKCYAN);
-    x0 = 180, y0 = 120, r0 = 40;
-    M5.Lcd.fillCircle(x0, y0, r0, DARKGREY);
-    M5.Lcd.setTextSize(1);
-    M5.Lcd.setTextColor(pid1.GetMode() ? GREEN : WHITE);
 
-    x0 = 120, y0 = 50, r0 = 60;
-    M5.Lcd.setFont(&FreeSansBold24pt7b);
-    M5.Lcd.setTextDatum(MR_DATUM);
-    sprintf(str, "%.1f", temp1);
-    M5.Lcd.drawString(str, x0 + 40, y0 + 5);
+    img.fillScreen(BLACK);
+    if (page == 0) // MAIN
+    {
 
-    x0 = 180, y0 = 120, r0 = 40;
-    M5.Lcd.setTextColor(pid1.GetMode() ? GREEN : WHITE);
+      int x0 = 120, y0 = 70, r0 = 70;
+      img.fillCircle(x0, y0, r0, pid1.GetMode() ? ORANGE : DARKGREY);
+      // img.setFont(&FreeSansBold24pt7b);
+      // img.setTextSize(2);
+      img.setTextColor(pid1.GetMode() ? WHITE : LIGHTGREY);
+      img.setFont(&fonts::FreeSansBold24pt7b);
 
-    M5.Lcd.setFont(&FreeSansBold18pt7b);
-    M5.Lcd.setTextDatum(MC_DATUM);
-    sprintf(str, "%.1f", tset1);
-    M5.Lcd.drawString(str, x0, y0 + 5);
-*/
-    // M5.Lcd.setFont(&FreeSansBold18pt7b);
-    M5.Lcd.setTextDatum(MC_DATUM);
-    M5.Lcd.fillScreen(BLACK);
+      img.setTextDatum(MC_DATUM);
+      sprintf(str, "%.1f", temp1);
+      img.drawString(str, x0, y0 + 5);
 
-    M5.Lcd.setFont(&FreeMonoBold12pt7b);
+      x0 = 180, y0 = 145, r0 = 35;
+      img.fillCircle(x0, y0, r0, DARKCYAN);
+      img.setTextSize(1);
+      // img.setTextColor(pid1.GetMode() ? GREEN : WHITE);
+      // img.setTextColor(pid1.GetMode() ? GREEN : WHITE);
 
-    M5.Lcd.setCursor(0, 90);
-    M5.Lcd.printf("%5.1f Set: %3.1f", temp1, tset1);
-    M5.Lcd.setTextSize(1);
-    int k = 130;
+      img.setFont(&FreeSansBold12pt7b);
+      img.setTextDatum(MC_DATUM);
+      sprintf(str, "%.1f", tset1);
+      img.drawString(str, x0, y0 + 5);
 
-    M5.Lcd.setFont(&FreeMonoBold12pt7b);
+      img.setTextColor(color565(50));
 
-    M5.Lcd.setCursor(0, k);
-    M5.Lcd.printf("%4.0f%%<%4.0f%%<%4.0f%%", pid1.GetOutMin(), output1, pid1.GetOutMax());
-    k += 20;
-    M5.Lcd.setCursor(35, k);
-    M5.Lcd.printf("GP: %7.2g", pid1.GetKp());
-    k += 20;
-    M5.Lcd.setCursor(35, k);
-    M5.Lcd.printf("GI: %7.2g", pid1.GetKi());
-    k += 20;
-    M5.Lcd.setCursor(35, k);
-    M5.Lcd.printf("GD: %7.2g", pid1.GetKd());
-    k += 20;
-    M5.Lcd.setCursor(35, k);
-    static float i_affiche = 0;
-    if (isens_HR >= i_affiche + 5 || isens_HR <= i_affiche - 5)
-      i_affiche = 0.001 * ((int)(isens_HR / 10));
-    M5.Lcd.printf(" I = %4.2f A", isens_HR/1000);
+      img.setTextColor(color565(50));
+      img.setTextDatum(middle_center);
+      img.setFont(&fonts::DejaVu18);
+      img.setTextSize(1);
+      x0 = M5Dial.Display.width() / 2;
+      y0 = 205;
 
+      if (pid1.GetMode() == 0)
+      {
+        img.drawString("OFF", x0, y0);
+      }
+      else
+      {
+        if (output1 < 0)
+        {
+          img.drawString("COOLING", x0, y0);
+          sprintf(str, "%.0f%%", 100 * fabs(output1 / pid1.GetOutMin()));
+          img.drawString(str, x0, y0 + 20);
+        }
+        else
+        {
+          img.drawString("HEATING", x0, y0);
+          sprintf(str, "%.0f%%", 100 * fabs(output1 / pid1.GetOutMax()));
+          img.drawString(str, x0, y0 + 20);
+        }
+      }
+    }
+    else if (page == 1) // Option
+    {
+      img.setTextDatum(MC_DATUM);
+      img.fillScreen(BLACK);
+
+      img.setFont(&FreeMonoBold12pt7b);
+
+      img.setCursor(0, 90);
+      img.printf("%5.1f Set: %3.1f", temp1, tset1);
+      img.setTextSize(1);
+      int k = 110;
+
+      img.setFont(&FreeMonoBold12pt7b);
+
+      img.setCursor(0, k);
+      img.printf("%4.0f%%<%4.0f%%<%4.0f%%", pid1.GetOutMin(), output1, pid1.GetOutMax());
+      k += 20;
+      img.setCursor(35, k);
+      img.printf("GP: %7.2g", pid1.GetKp());
+      k += 20;
+      img.setCursor(35, k);
+      img.printf("GI: %7.2g", pid1.GetKi());
+      k += 20;
+      img.setCursor(35, k);
+      img.printf("GD: %7.2g", pid1.GetKd());
+      k += 20;
+      img.setCursor(35, k);
+      static float i_affiche = 0;
+      // if (isens_HR >= i_affiche + 5 || isens_HR <= i_affiche - 5)
+      // i_affiche = 0.001 * ((int)(fabs(isens_HR) / 10));
+      img.printf(" I = %4.2f A", fabs(isens_HR) / 1000);
+
+      k += 20;
+      img.setCursor(35, k);
+      float i_peltier = 0;
+      if (fabs(output1) >= 4)
+        i_peltier = fabs(isens_HR / pow(fabs(output1) / 95.0, 0.8) / 1000);
+      img.printf(" I = %4.2f A", i_peltier);
+    }
     /*
-        M5.Lcd.setTextColor(PINK, BLACK);
-        M5.Lcd.setCursor(0, 100);
-        M5.Lcd.printf("  T2 = %7.2fC", temp2);
+        img.setTextColor(PINK, BLACK);
+        img.setCursor(0, 100);
+        img.printf("  T2 = %7.2fC", temp2);
 
-        M5.Lcd.setCursor(0, 130);
-        M5.Lcd.printf("Set2 = %7.2fC", tset2);
+        img.setCursor(0, 130);
+        img.printf("Set2 = %7.2fC", tset2);
 
-        M5.Lcd.setCursor(0, 160);
-        M5.Lcd.printf("Out2 = %7.1f%", output2);
+        img.setCursor(0, 160);
+        img.printf("Out2 = %7.1f%", output2);
     */
     /// Handle Butons
 
@@ -125,11 +186,27 @@ void loopGUI(void *param)
        clear_encoder();
      }*/
 
-    tset1 += get_encoder() * 0.1f;
+#ifdef DEBUG
+    output1 += get_encoder();
+    output1 = constrain(output1, -100, 100);
+#else
+    tset1 += get_encoder() * 0.125f;
+#endif
+
     clear_encoder();
 
     if (M5.BtnA.wasPressed())
-      pid1.SetMode(!pid1.GetMode()); //
+    {
+      page++;
+      if (page == 2)
+        page = 0;
+    }
+
+    if (buttonScreen.wasPressed())
+    {
+      outputEnable = !outputEnable;
+      pid1.SetMode(outputEnable);
+    }
 
     /*if (M5.BtnB.wasPressed() || M5.BtnB.pressedFor(200))
       tset1 -= 0.1;
@@ -139,6 +216,7 @@ void loopGUI(void *param)
     if (M5.BtnB.wasReleased() || M5.BtnC.wasReleased())
       save_value("PID1", "TSET", tset1);*/
 
+    img.pushSprite(0, 0);
     delay(100);
   }
 }
@@ -159,26 +237,29 @@ void loopServo(void *param)
     // x = analogRead(36) * T_factor;
     // temp2 = alpha * x + (1 - alpha) * temp2;
 
+#ifndef DEBUG
     pid1.Compute();
+#endif
     // pid2.Compute();
 
     if (outputEnable)
     {
       if (output1 >= 0)
       {
-        ledcWrite(0, fabs(output1 / 100.0) * 2047);
-        ledcWrite(1, 0);
+        // ledcWrite(0, 0);
+        gpio_set_level(Pin_DIR, 0);
       }
       else
       {
-        ledcWrite(0, 0);
-        ledcWrite(1, fabs(output1 / 100.0) * 2047);
+        gpio_set_level(Pin_DIR, 1);
+        // ledcWrite(0, 1);
       }
+      ledcWrite(0, MaxPWM * (1 - fabs(output1 / 100.0)));
     }
     else
     {
-      ledcWrite(0, 0);
-      ledcWrite(1, 0);
+      // ledcWrite(0, 0);
+      ledcWrite(0, MaxPWM);
     }
 
     delay(100);
@@ -198,18 +279,20 @@ void setup()
 
   init_encoder();
 
-  M5.Lcd.setFont(&FreeMonoBold18pt7b);
+  // M5.Lcd.setFont(&FreeMonoBold18pt7b);
   ds.setResolution(12);
 
-  pinMode(Pin_PWM1, OUTPUT);
-  pinMode(Pin_PWM2, OUTPUT);
+  gpio_set_level(Pin_PWM, 1);
+  pinMode(Pin_DIR, OUTPUT);
+  pinMode(Pin_PWM, OUTPUT);
 
   ledcSetup(0, freqPWM, NbitPWM);
-  ledcSetup(1, freqPWM, NbitPWM);
+  // ledcSetup(1, freqPWM, NbitPWM);
 
   // ledcAttachPin(out1Pin[0], 0);
-  ledcAttachPin(Pin_PWM1, 0);
-  ledcAttachPin(Pin_PWM2, 1);
+  // ledcAttachPin(Pin_DIR, 0);
+  ledcAttachPin(Pin_PWM, 0);
+  ledcWrite(0, MaxPWM);
 
   adcAttachPin(ISensPin);
   // pinMode(ISensPin, INPUT);
@@ -245,16 +328,16 @@ void setup()
   Serial.println(pid1.GetMode());
 
   pid1.SetMode(MANUAL);
+  outputEnable = false;
   // pid2.SetMode(MANUAL);
 }
 
-void loop() 
+void loop()
 {
 
   const u32_t N_Sample = 50;     // Average over N_Sample samples for fast lock
   const u32_t N_Sample_HR = 500; // Average over N_Sample_HR samples for long term regulation
-  const float RSENS = 0.1;       // 100mOhm Rsens
-
+  const float RSENS = 0.10;      // 100mOhm Rsens
 
   // Make 3 measurments and keep the median
   float a = analogReadMilliVolts(ISensPin);
@@ -275,10 +358,12 @@ void loop()
   else
     measure = a;
 
-  // measure = analogReadMilliVolts(ISensPin);
-  // measure = analogRead(ISensPin);
+  if (!outputEnable)
+  { // refresh the offset
+    V_offset = 0.9 * V_offset + 0.1 * measure;
+  }
 
-  float Imeas = measure / RSENS;
+  float Imeas = (measure - V_offset) / RSENS;
 
   isens = ((N_Sample - 1) * isens + Imeas) / N_Sample;
   // isens = measure;
